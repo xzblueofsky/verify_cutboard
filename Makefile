d8c7be34e13c8085aab8684e1b14906114192cb3
zhao xin <xinzhao@deepglint.com>
2015-10-19 20:33:18 +0800

    empty base

---
+++ Makefile
@@ -0,0 +1,2 @@
+TARGET: main.cpp
+	g++ main.cpp -lopencv_core -lopencv_highgui
---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  verify cutboard image
+ *
+ *        Version:  1.0
+ *        Created:  2015年10月19日 20时29分47秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  XinZhao (zx), xinzhao@deepglint.com
+ *        Company:  DeepGlint
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+int main( int argc, char* argv[]) {
+	printf("hello world\n");
+	return 0;
+}
